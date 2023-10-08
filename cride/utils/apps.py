e79c4922f83b8322661f1bb23df71a1e353c30e8
3dwintny <tecnotony8@gmail.com>
2023-10-08 12:35:29 -0600

    Modulo 3 y 4

---
+++ cride/utils/apps.py
@@ -0,0 +1,10 @@
+""" Users app."""
+
+# Django 
+from django import AppConfig 
+
+class UsersAppConfig(AppConfig):
+    """Users app config."""
+    
+    name = 'cride.users'
+    verbose_name = 'users'
